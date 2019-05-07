0abab34a3e6f06619881f538c492f7f896f8e2da
paguerre2 <Pablo_Aguerre@Mcafee.com>
2019-05-07 19:32:59 -0300

    travis template setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+PACKAGES = find_packages()
+
+opts = dict(name="activityfeed-tests",
+            maintainer="Pablo Aguerre",
+            maintainer_email="Pablo_Aguerre@McAfee.com",
+            description="Open Source ActivityFeed integrated with OpenDXL streaming clien",
+            long_description="Open Source ActivityFeed integrated with OpenDXL streaming client (https://github.com/opendxl/opendxl-streaming-client-python). It includes Client samples that shares thread-data between MVISION EDR and SIEM OnPrem.",
+            url="https://github.com/mcafee/mvision-edr-activity-feed",
+            download_url="https://github.com/mcafee/mvision-edr-activity-feed",
+            license="MIT",
+            packages=PACKAGES)
+
+
+if __name__ == '__main__':
+    setup(**opts)
