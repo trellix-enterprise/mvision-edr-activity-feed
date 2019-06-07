fd9edc88590739827ffb771e1d25db159bd0c4ba
paguerre2 <50333755+paguerre2@users.noreply.github.com>
2019-06-07 15:58:40 -0300

    sys log local comments neede for code review

--- samples/esm/__init__.py
+++ samples/esm/__init__.py
@@ -1,22 +1,32 @@
+import requests
+from logging.handlers import SysLogHandler
 import logging
 import json
-import requests
 
 class EsmEvents(object):
 
     def __init__(self, url):
         self.url = url
-        self.user = None
-        self.password = None
-
+        # SysLog support at Sample level -Perhaps is better at global level.
+        # SysLogHandler configured to local syslog and it requires that the
+        # user configures rsyslog.conf to point to remote ESM because:
+        # 1. TLS can be configured from local sys log to remote ESM (In order to avoid Man in the Middle Attack)
+        # 2. More performant as rsyslog can be configured to send batches
+        # Anyways, ut the SDK client developer decides to not poin tolcally simply pass url to SysLogHandler: 
+        # handler = SysLogHandler(address=('ESM_REMOTE_SERVER_IP', UDP_OR_TLS_PORT))
+        logger = logging.getLogger()
+        handler = SysLogHandler(address='/dev/log')
+        # Logging level must be info for SysLogHandler in order to send proper information:
+        handler.setLevel(logging.INFO)
+        logger.addHandler(handler)
+        # Default log location in case needed at sudo level for investigating payloads.
+        # Normally commented as it needs to be run as sudo:
+        logger.addHandler(logging.FileHandler("/var/log/mvedr_activity_feed.log"))
+        
     def case_created(self, event):
-        logging.info("Sending case...")
-        #sys-log configured internally using rsyslog:
+        logging.debug(">>>Sending ESM case...")
         logging.info(json.dumps(event))
-        #body = json.dumps(event)
-        #requests.post(self.url, data=body)
-
+        
     def threat_created(self, event):
-        logging.info("Sending threat...")
+        logging.debug(">>>Sending ESM threat...")
         logging.info(json.dumps(event))
-        #requests.post(self.url, data=body)
