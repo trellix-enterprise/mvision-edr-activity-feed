43c43a9691592ec5e5da80ee2c22cd627ab62a96
paguerre2 <50333755+paguerre2@users.noreply.github.com>
2019-06-07 17:25:28 -0300

    Update __init__.py

--- samples/esm/__init__.py
+++ samples/esm/__init__.py
@@ -5,8 +5,10 @@ class EsmEvents(object):
         
     def send_case(self, event):
         logging.debug(">>>Sending ESM case...")
+        # It uses SysLogHandler configured in esm main module so we only need to dump the Case event:
         logging.info(json.dumps(event))
         
     def send_threat(self, event):
+        # It uses SysLogHandler configured in esm main module so we only need to dump the Threat event:
         logging.debug(">>>Sending ESM threat...")
         logging.info(json.dumps(event))
