0c5db35aa00a942484324717756d21e22d8290db
paguerre2 <50333755+paguerre2@users.noreply.github.com>
2019-08-27 17:01:03 -0300

    Add files via upload

---
+++ samples/slack.py
@@ -0,0 +1,80 @@
+"""
+MCAFEE CONFIDENTIAL
+Copyright (c) 2019 McAfee, LLC
+The source code contained or described herein and all documents related
+to the source code ("Material") are owned by McAfee or its
+suppliers or licensors. Title to the Material remains with McAfee
+or its suppliers and licensors. The Material contains trade
+secrets and proprietary and confidential information of McAfee or its
+suppliers and licensors. The Material is protected by worldwide copyright
+and trade secret laws and treaty provisions. No part of the Material may
+be used, copied, reproduced, modified, published, uploaded, posted,
+transmitted, distributed, or disclosed in any way without McAfee's prior
+express written permission.
+No license under any patent, copyright, trade secret or other intellectual
+property right is granted to or conferred upon you by disclosure or
+delivery of the Materials, either expressly, by implication, inducement,
+estoppel or otherwise. Any license under such intellectual property rights
+must be express and approved by McAfee in writing.
+"""
+
+"""
+Example of subscription to individual case-related events
+"""
+import logging
+from samples.slack import SlackEvents
+from mvision_edr_activity_feed import subscribe
+
+
+slack_events = None
+
+
+def setup(config):
+    """Setup global connection to The hive using callback configuration
+    parameters:
+    * slack_token: Slack token
+    * bot_user: User used by this script to post messages in Slack
+    * default_user: Default user to invite to channels when no mapping is found
+    * activity_channel: Channel in which to post summary
+    * ui_url: URL of the Investigator UI
+    """
+    global slack_events
+    if not slack_events:
+        slack_events = SlackEvents(config.slack_token, config.ui_url,
+                                   config.bot_user, config.activity_channel,
+                                   lambda user: config.default_user)
+
+
+@subscribe(entity='case', subtype='creation')
+def case_created(event, config):
+    logging.info("CASE CREATED: %s", event)
+    setup(config)
+    slack_events.case_created(event)
+
+
+@subscribe(entity='case', subtype='priority-update')
+def case_updated(event, config):
+    logging.info("CASE PRIORITY UPDATED: %s", event)
+    setup(config)
+    slack_events.case_priority_updated(event)
+
+
+@subscribe(entity='case', subtype='status-update')
+def case_status_updated(event, config):
+    logging.info("CASE STATUS UPDATED: %s", event)
+    setup(config)
+    slack_events.case_status_updated(event)
+
+
+@subscribe(entity='case', subtype='select')
+def case_select(event, config):
+    logging.info("CASE SELECT: %s", event)
+    setup(config)
+    slack_events.case_selected_ui(event)
+
+
+@subscribe(entity='case', subtype='unselect')
+def case_unselect(event, config):
+    logging.info("CASE UNSELECT: %s", event)
+    setup(config)
+    slack_events.case_unselected_ui(event)
